5a3f791d7353bd25b6994b8dd29e60190caa91af
ArshiaAHP <diniodduck@gmail.com>
2025-06-10 18:02:49 +0330

    add User class to model.

---
+++ src/main/java/aut/ap/model/User.java
@@ -0,0 +1,43 @@
+package aut.ap.model;
+
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "sender")
+    private List<Email> sentEmails = new ArrayList<>();
+
+    @OneToMany(mappedBy = "recipient")
+    private List<EmailRecipient> receivedEmails = new ArrayList<>();
+
+    public User() {}
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+}
+
