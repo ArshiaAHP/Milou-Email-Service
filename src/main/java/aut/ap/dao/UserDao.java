bae188e571d9d4e59dd046783cca889c7a5cf4d8
ArshiaAHP <diniodduck@gmail.com>
2025-06-10 19:39:16 +0330

    add UserDao.

---
+++ src/main/java/aut/ap/dao/UserDao.java
@@ -0,0 +1,28 @@
+package aut.ap.dao;
+
+import aut.ap.model.User;
+import aut.ap.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+public class UserDao {
+    public void saveUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to save user: " + e.getMessage(), e);
+        }
+    }
+
+    public User findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("FROM User WHERE email = :email", User.class);
+            query.setParameter("email", email);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find user by email: " + e.getMessage(), e);
+        }
+    }
+}
